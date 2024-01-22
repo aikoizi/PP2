6dc4e7b1a765051b36021f406396675e6c1d2922
admin <ajgerim.izimbetova@gmail.com>
2024-01-23 03:19:25 +0600

    Some upgread

---
+++ lab1/numbers.py
@@ -0,0 +1,11 @@
+#Exercise1
+x = 5
+x = float(x)
+
+#Exercise2
+x = 5.5
+x = int(x)
+
+#Exercise3
+x = 5
+x = complex(x)
---
+++ lab1/strings.py
@@ -0,0 +1,36 @@
+#Exercise1
+x = "Hello World"
+print(len(x))
+
+#Exercise2
+txt = "Hello World"
+x = txt[0]
+
+#Exercise3
+txt = "Hello World"
+x = txt[2:5]
+
+#Exercise4
+txt = " Hello World "
+x = txt.strip()
+
+#Exercise5
+txt = "Hello World"
+txt = txt.upper()
+
+#Exercise6
+txt = "Hello World"
+txt = txt.lower()
+
+#Exercise7
+txt = "Hello World"
+txt = txt.replace("H", "J")
+
+#Exercise8
+age = 36
+txt = "My name is John, and I am {}"
+print(txt.format(age))
+
+
+
+
