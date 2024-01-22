6b80e2b46e9e2c08ab90190c8a3a79976ad671cb
admin <ajgerim.izimbetova@gmail.com>
2024-01-22 23:58:17 +0600

    some upgread

---
+++ lab1/datatypes.py
@@ -0,0 +1,40 @@
+#Exercise1
+x = 5
+print(type(x))
+#Answer = int
+
+#Exercise2
+x = "Hello World"
+print(type(x))
+#Answer = str
+
+#Exercise3
+x = 20.5
+print(type(x))
+#Answer = float
+
+#Exercise4
+x = ["apple", "banana", "cherry"]
+print(type(x))
+#Answer = list
+
+#Exercise5
+x = ("apple", "banana", "cherry")
+print(type(x))
+#Answer = tuple
+
+#Exercise6
+x = {"name" : "John", "age" : 36}
+print(type(x))
+#Answer = dict
+
+#Exercise7
+x = True
+print(type(x))
+#Answer = bool
+
+
+
+
+
+
