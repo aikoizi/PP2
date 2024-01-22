badd352dae907d9768e04001d8cf9153fcdab1f3
admin <ajgerim.izimbetova@gmail.com>
2024-01-22 23:47:13 +0600

    some upgrade

---
+++ lab1/variables.py
@@ -0,0 +1,27 @@
+#Exersies1
+carname = "Volvo"
+
+#Exersies2
+x = 50
+
+#Exersies3
+x = 5
+y = 10
+print(x +y)
+
+#Exersies4
+x = 5
+y = 10
+z = x + y
+print(z)
+
+#Exersies5
+x,y,z = "Orange", "Banana", "Cherry"
+
+#Exersies6
+x = y = z = "Orange"
+
+#Exersies7
+def myfunc():
+  global x
+  x = "fantastic"
