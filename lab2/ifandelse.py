640d2b09b39707ed04a0a8cc07e2dd26dde09a76
admin <ajgerim.izimbetova@gmail.com>
2024-01-24 10:44:38 +0600

    Some upgerad

---
+++ lab2/ifandelse.py
@@ -0,0 +1,56 @@
+#Exercise1
+a = 50
+b = 10
+if a > b:
+    print("Hello World")
+
+#Exercise2
+a = 50
+b = 10
+if a != b:
+   print("Hello World")
+
+#Exercise3
+a = 50
+b = 10
+if a == b:
+    print("Yes")
+else:
+    print("No")
+
+#Exercise4
+a = 50
+b = 10
+if a == b:
+    print("1")
+elif a > b:
+    print("2")
+else:
+    print("3")
+
+#Exercise5
+c = 5
+d = 5
+if a == b and c == d:
+    print("Hello")
+
+#Exercise6
+if a == b or c == d:
+    print("Hello")
+
+#Exercise7
+if 5 > 2:
+    print("YES")
+
+#Exercise8
+a = 2
+b = 5
+print("YES") if a == b else print("NO")
+
+#Exercise9
+a = 2
+b = 50
+c = 2
+if a == c or b == c:
+    print("YES")
+
